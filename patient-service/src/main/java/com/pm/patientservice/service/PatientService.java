7595bce51aa81f221b283cd8df9f162390bdd023
oybek-olimov <oybekolimov001@gmail.com>
2025-07-06 23:47:23 +0500

    .

---
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -0,0 +1,34 @@
+package com.pm.patientservice.service;
+
+import com.pm.patientservice.dto.PatientRequestDTO;
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.mapper.PatientMapper;
+import com.pm.patientservice.model.Patient;
+import com.pm.patientservice.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PatientService {
+
+    private final PatientRepository patientRepository;
+
+    public PatientService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    public List<PatientResponseDTO> getPatients() {
+        List<Patient> patients = patientRepository.findAll();
+        return patients.stream()
+                .map(PatientMapper::toDTO)
+                .toList();
+    }
+
+    public PatientResponseDTO createPatient(PatientRequestDTO requestDTO) {
+        Patient newPatient = patientRepository.save(PatientMapper.toModel(requestDTO));
+        return PatientMapper.toDTO(newPatient);
+    }
+}
